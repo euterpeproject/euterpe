d7446ea116c8e462681666e8b5a92e91ce0fa45a
victor <vitr2o@gmail.com>
2022-02-15 13:01:17 -0500

    Cambio código y estilo se agrega carpeta js

---
+++ js/contador.js
@@ -0,0 +1,19 @@
+document.write('<div class="cuenta"><span class="contador">');
+
+if (localStorage.pagecount) {
+    localStorage.pagecount = Number(localStorage.pagecount) + 1;
+} else {
+    localStorage.pagecount = 1;
+}
+document.write("Has visitado mi página " + localStorage.pagecount + " veces");
+
+
+if (sessionStorage.pagecount) {
+    sessionStorage.pagecount = Number(sessionStorage.pagecount) + 1;
+} else {
+    sessionStorage.pagecount = 1;
+}
+document.write("En esta sesión, has visitado mi página :" + sessionStorage.pagecount + " veces");
+
+document.write('</span></div>');
+
