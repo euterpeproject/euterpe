35961bbea8866819826aa316bdab2dd1ccdedfdb
victor <vitr2o@gmail.com>
2022-04-03 23:13:44 -0500

    Se cambia estilo y se agrega archivo php

--- php/enviamail.php
+++ php/enviamail.php
@@ -5,7 +5,7 @@
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 		<meta name="description" content="Euterpe Death" />
 		<meta name="keywords" content="Weblog, blog, Euterpe Death, Musica, Metal" />
-		<link rel="stylesheet" href="css/estilo.css" type="text/css" />
+		<link rel="stylesheet" href="../css/estilo.css" type="text/css" />
 	</head>
 	<body>
 		<div id="cajaheader">
@@ -172,76 +172,76 @@
 					</svg>
 					
 				</div>
-
 				<div>
-					<h1>EUTERPE</h1>
+				<h1>EUTERPE</h1>
 				</div>
-				
-			</header>
-
-			<div id="contenido">
-				<hr>
-				<center><nav>
+				<nav>
 					<ul>
-						<li><a href="index.html"><canvas id="iconoinicio" width="50" height="45"></canvas><br />Inicio</a></li>
-						<li><a href="musica.html"><canvas id="iconomusica" width="50" height="45"></canvas><br />Música</a></li>
-						<li><a href="sobremi.html"><canvas id="iconosobreMi" width="50" height="45"></canvas><br />Sobre mi</a></li>
-						<li><a href="contacto.html"><canvas id="iconocontacto" width="50" height="45"></canvas><br />Contacto</a></li>
+						<li><a href="../index.html"><canvas id="iconoinicio" width="50" height="50"></canvas><br />Inicio</a></li>
+						<li><a href="../musica.html"><canvas id="iconomusica" width="50" height="50"></canvas><br />Música</a></li>
+						<li><a href="../sobremi.html"><canvas id="iconosobreMi" width="50" height="50"></canvas><br />Sobre mi</a></li>
+						<li><a href="../contacto.html"><canvas id="iconocontacto" width="50" height="50"></canvas><br />Contacto</a></li>
 					</ul>
-				</nav></center>
-				<hr>
-				<form action="php/enviamail.php" method="post">
-					<table border=0>
-						<tr>
-							<td width=50%>
-								<p class="pform">Ingresa tu nombre</p>
-							</td>
-							<td width=50%>
-								<input type="text" width=100% required name="nombre"/>
-							</td>
-						</tr>
-						<tr>
-							<td>
-								<p class="pform">Ingresa tu email</p>
-							</td>
-							<td>
-								<input type="email" width=100% value="ejemplo: usuario@dominio.com" name="email"/>
-							</td>
-						</tr>
-						<tr>
-							<td>
-								<p class="pform">Escribe tu mensaje</p>
-							</td>
-							<td>
-								<textarea cols=50% rows=6 name="mensaje"></textarea>
-							</td>
-						</tr>
-						<tr>
-							<td>
-							</td>
-							<td>
-								<input type="submit" value="Enviar">
-							</td>
-						</tr>
-					</table>
-				</form>
-			</div>
+				</nav>
+			</header>
+		
+			<div id="contenido">
+				<div id="php">
+				<?php
 
+//Recibo las variables de formulario
+
+$nombre = $_POST['nombre'];
+$mail = $_POST['email'];
+$mensaje = $_POST['mensaje'];
+
+//Te muestro las variables
+
+echo "<br />";
+echo "<h3>El mensaje que has enviado es:</h3>";
+echo "<br />";
+echo "<h4>Nombre: ";
+echo $nombre;
+echo "<br />";
+echo "Email: ";
+echo $mail;
+echo "<br />";
+echo "Mensaje: ";
+echo $mensaje;
+echo "</h4>";
+
+//Envio un email
+
+$aquien = "vitr2o@gmail.com";
+$asunto = "Has recibido un correo del Blog";
+$mensajemail = $nombre." con el email ".$mail." te ha enviado un mensaje que dice ".$mensaje;
+
+if(mail($aquien,$asunto,$mensaje)){
+
+echo "Tu email se ha enviado correctamente";
+
+}else{
+echo "El envio del mail ha fallado";
+}
+ 
+
+?>
+				</div>
+			</div>
 			<footer>
-                <h6>
-                    <div class="vcard">
+				<h6>
+					<div class="vcard">
                         <div> <span class="name">Euterpe Death</span>-<span class="locality">Medellín</span>-<span class="region">Antioquia</span>-<span class="country-name">Colombia</span></div>
                         <div> <span class="tel">000 000 00 00 - 000 00 00</span>-<span class="email">vitr2o@gmail.com</span></div>
                     </div>
-                </h6>
+				</h6>
 
-                <script src="js/contador.js"></script> 
+				<script src="../js/contador.js"></script> 
 
-            </footer>
-
+			</footer>
 		</div>
 
-		<script src="js/iconos.js"></script>
+		<script src="../js/iconos.js"></script>
 
 	</body>
 </html>
