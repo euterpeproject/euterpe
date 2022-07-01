9166c3c41210de500e2a65aa73ed9f7da7934498
euterpe <vitr2o@gmail.com>
2022-07-01 18:53:42 -0500

    actualización envío de formulario

--- php/enviamail.php
+++ php/enviamail.php
@@ -79,7 +79,7 @@
 				$mail->Host       = 'smtp.gmail.com';                       
 				$mail->SMTPAuth   = true;                                   
 				$mail->Username   = 'vitr2o@gmail.com';                     
-				$mail->Password   = 'uijjiojcvbyimjyy';                               
+				$mail->Password   = '';                               
 				$mail->SMTPSecure = 'tls';                                  
 				// $mail->Port       = 465;                                 
 				$mail->Port       = 587;                                        
